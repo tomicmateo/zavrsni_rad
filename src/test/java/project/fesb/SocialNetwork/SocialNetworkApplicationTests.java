9f62b91e944714e1ef22c76fb13f36f434440cc1
tomicmateo <Mateo.Tomic.02@fesb.hr>
2024-04-10 13:23:59 +0200

    Initial commit

---
+++ src/test/java/project/fesb/SocialNetwork/SocialNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package project.fesb.SocialNetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialNetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
